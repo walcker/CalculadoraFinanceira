468be20b0bbd12f2143c74d85ebc49105f5036d9
nmpo <101530066+nmpo@users.noreply.github.com>
2022-07-03 20:45:33 -0300

    feat: Create CDBeLC.java

---
+++ src/main/java/br/ufrn/imd/CDBeLC.java
@@ -0,0 +1,17 @@
+package br.ufrn.imd;
+
+public class CDBeLC extends Investimento{
+    public double taxaCDI = 13.15;
+    public double CDBeLC = taxaCDI * 1.27;
+
+    /**
+     * @param CDBLC rentabilidade 124% da CDI
+     * @param valorInicial valor inicial investido
+     * @return valor final do investimento
+     */
+    @Override
+    public double calcular(double CDBLC, double valorInicial) {
+        return (CDBLC) / 100 * valorInicial * (float) (qntMeses / 12) + valorInicial;
+    }
+
+}
