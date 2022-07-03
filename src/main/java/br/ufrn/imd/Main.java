81d11f8db7740198caa2b73633fd3ba0f7a66d5b
nmpo <101530066+nmpo@users.noreply.github.com>
2022-07-03 20:59:47 -0300

    feat: update instancias e sout.

--- src/main/java/br/ufrn/imd/Main.java
+++ src/main/java/br/ufrn/imd/Main.java
@@ -6,21 +6,43 @@ public class Main {
 
         TesouroSelic selic = new TesouroSelic();
         selic.valorInicial = 5000;
-        selic.rentabilidadeLiquidaAno = 11.45;
         selic.valorMensal = 200;
         selic.qntMeses = 60;
 
         TesouroIPCA ipca = new TesouroIPCA();
         ipca.valorInicial = 5000;
-        ipca.IPCA = 10.45;
         ipca.valorMensal = 200;
         ipca.qntMeses = 60;
 
-        System.out.println("Resgate do investimento de R$ 5.000,00 no Tesouro Selic.");
-        System.out.println(selic.calcular(selic.rentabilidadeLiquidaAno, selic.valorInicial));
+        TesouroPrefixado pref = new TesouroPrefixado();
+        pref.valorInicial = 5000;
+        pref.valorMensal = 200;
+        pref.qntMeses = 60;
 
-        System.out.println("Resgate do investimento de R$ 5.000,00 no Tesouro IPCA.");
+        Poupanca poupanca = new Poupanca();
+        poupanca.valorInicial = 5000;
+        poupanca.valorMensal = 200;
+        poupanca.qntMeses = 60;
+
+        CDBeLC cdblc = new CDBeLC();
+        cdblc.valorInicial = 5000;
+        cdblc.valorMensal = 200;
+        cdblc.qntMeses = 60;
+
+        System.out.println("Resgate do investimento de R$ " + selic.valorInicial + " no Tesouro Selic com rendimento de " + selic.Selic + "% a.a..");
+        System.out.println(selic.calcular(selic.Selic, selic.valorInicial));
+
+        System.out.println("Resgate do investimento de R$ " + ipca.valorInicial + " no Tesouro IPCA com rendimento de " + ipca.IPCA + "% a.a. e prefixado.");
         System.out.println(ipca.calcular(ipca.IPCA, ipca.valorInicial));
+
+        System.out.println("Resgate do investimento de R$ " + pref.valorInicial + " no Tesouro Prefixado com rendimento de " + pref.prefixado + "% a.a..");
+        System.out.println(pref.calcular(pref.prefixado, pref.valorInicial));
+
+        System.out.println("Resgate do investimento de R$ " + poupanca.valorInicial + " na Poupança com rendimento de " + poupanca.Poupanca + "% a.a..");
+        System.out.println(poupanca.calcular(poupanca.Poupanca, poupanca.valorInicial));
+
+        System.out.println("Resgate do investimento de R$ " + cdblc.valorInicial + " no CDB e LC com rendimento de " + cdblc.CDBeLC + "% a.a..");
+        System.out.println(cdblc.calcular(cdblc.CDBeLC, cdblc.valorInicial));
 
     }
 }
