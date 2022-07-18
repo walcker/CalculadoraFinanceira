d32a4540373e44e2275e750718769c956bd0e720
nmpo <101530066+nmpo@users.noreply.github.com>
2022-07-17 22:02:15 -0300

    feat: add javadoc and try cast

--- src/main/java/br/ufrn/imd/Investimento.java
+++ src/main/java/br/ufrn/imd/Investimento.java
@@ -7,8 +7,19 @@ public abstract class Investimento {
     public double valorMensal;
     public int qntMeses;
 
-
+    /**
+     * @param indice rentabilidade do investimento
+     * @param valorInicial valor inicial investido
+     * @return valor final do investimento
+     */
     public double calcular(double indice, double valorInicial){
+        try {
+            if (valorInicial <= 0) {
+                throw new IllegalArgumentException("Valor inicial deve ser maior que zero.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
         return ((indice)/100 * valorInicial) * (float) (qntMeses/12) + valorInicial;
     }
 
