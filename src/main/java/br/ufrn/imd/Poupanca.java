ddf73a0c5f3b9ddaf6d80347fb72a821f7297048
nmpo <101530066+nmpo@users.noreply.github.com>
2022-07-17 22:03:17 -0300

    feat: add try cast

--- src/main/java/br/ufrn/imd/Poupanca.java
+++ src/main/java/br/ufrn/imd/Poupanca.java
@@ -11,6 +11,13 @@ public class Poupanca extends Investimento {
      */
     @Override
     public double calcular(double Poupanca, double valorInicial) {
+        try {
+            if (valorInicial <= 0) {
+                throw new IllegalArgumentException("Valor inicial deve ser maior que zero.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
         return ((Poupanca) / 100 * valorInicial * (float) (qntMeses / 12) + valorInicial) + valorMensal * (qntMeses - 1);
     }
 
