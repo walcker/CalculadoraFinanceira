23e522ddbf3521efc8a717b23a16b690e4119b3a
nmpo <101530066+nmpo@users.noreply.github.com>
2022-07-17 22:04:38 -0300

    feat: add try cast

--- src/main/java/br/ufrn/imd/TesouroPrefixado.java
+++ src/main/java/br/ufrn/imd/TesouroPrefixado.java
@@ -11,6 +11,13 @@ public class TesouroPrefixado extends Investimento {
      */
     @Override
     public double calcular(double prefixado, double valorInicial){
+        try {
+            if (valorInicial <= 0) {
+                throw new IllegalArgumentException("Valor inicial deve ser maior que zero.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
         return ((prefixado/100 * valorInicial) * (float) (qntMeses/12) + valorInicial+valorMensal*(qntMeses-1));
     }
 
